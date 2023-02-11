055a7471c5675ef1a5edda52eab07abcf354d1a3
yaminiShrivastava <yamini8383@gmail.com>
2023-02-11 15:03:17 +0530

    first commit

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -2,10 +2,10 @@
  import './Carousel.css'
  export default function Carousel({Images}){
     const [current,setcurrent]=useState(0)
-    const [autoplay,setautoplay]=useState(true)
-    let timeout=null
+    // const [autoplay,setautoplay]=useState(true)
+    // let timeout=null
     useEffect(()=>{
-        timeout=autoplay&& setTimeout(incr, 2500);
+        setTimeout(incr, 2500);
     })
     
     const incr=()=>{
@@ -14,7 +14,9 @@
     }
     return (
         <div className='Carousel'>
-            <div className='carousel_wrapper' onMouseEnter={()=>{setautoplay(false);clearTimeout(timeout)}} onMouseLeave={()=>setautoplay(true)}>
+            <div className='carousel_wrapper' 
+            // onMouseEnter={()=>{setautoplay(false);clearTimeout(timeout)}} onMouseLeave={()=>setautoplay(true)}
+            >
                 {
                 Images.map((i,ind)=>{
                 return <div key={ind} className={ind===current?"carousel_card carousel_card_active":"carousel_card"}><img src={i.images} alt=""></img></div>
